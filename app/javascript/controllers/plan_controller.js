002867755d6d7b616c32ad592a70c73ef37c95b3
Aaron <aaron.truong.goldenowl@gmail.com>
2023-06-29 09:26:04 +0700

    Plan filters refactor: comments UI feat: plans filter UI

--- app/javascript/controllers/plan_controller.js
+++ app/javascript/controllers/plan_controller.js
@@ -55,4 +55,8 @@ export default class extends Controller {
       },
     });
   }
+
+  search(event) {
+    alert(1)
+  }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   devise_scope :user do
-    get 'users/index', to: 'users#index'
-    get 'users/index/:id', to: "users#show"
+    get '/users', to: 'users#index'
+    get '/users/:id', to: "users#show"
     post '/users/index_follow', to:'users#follow'
     delete '/users/index_unfollow', to:'users#unfollow'
     get '/users/sign_out', to: 'devise/sessions#destroy'
