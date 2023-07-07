26647d61316ab09ffc3d2b0cf87b430f2e5b7c0a
Aaron <aaron.truong.goldenowl@gmail.com>
2023-07-07 17:23:14 +0700

    Render-deploy refactor:  setup mail confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,8 +73,18 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000, from: ENV['GMAIL_USER_NAME'] }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }  
+
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['GMAIL_USER_NAME'], # This is the string literal 'apikey', NOT the ID of your API key
+    password: ENV['GOOGLE_PASSWORD_MAIL'], # This is the secret sendgrid API key which was issued during API key creation
+    domain: 'gmail.com',
+    port: 587,
+    address: 'smtp.gmail.com',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
