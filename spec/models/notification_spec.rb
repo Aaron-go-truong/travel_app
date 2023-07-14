3d86e4a2140b285c493a7a3d9b6691055baced72
Aaron <aaron.truong.goldenowl@gmail.com>
2023-07-14 16:16:42 +0700

    Rspec Testcases feat: notification model test

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it "should belongs to recipient" do
+      assc = described_class.reflect_on_association(:recipient)
+      expect(assc.macro).to eq :belongs_to
+    end
+  end
+
+  describe 'gem integration' do
+    it 'includes the Noticed::Model module' do
+      expect(Notification.ancestors).to include(Noticed::Model)
+    end
+  end
 end
